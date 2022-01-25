0ba3ec48ca1ae4f76b68db5e6acd6ea2d4ab0b7a
Fotios Tsakiris <footios76@gmail.com>
2022-01-25 05:39:05 +0200

    add Login comp. basic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./components/Login";
 import "./App.css";
 
 function App() {
-  return <div className="App">Learn React</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
